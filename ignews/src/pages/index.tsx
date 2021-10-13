c7ece7ba28ca7a4d0844515e482661d5c76648bd
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-10-12 21:48:22 -0300

    Página home

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,12 +1,22 @@
 import Head from 'next/head'
-
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <>
     <Head>
         <title>Início | ig.news</title>
-      </Head>
-    <h1> Hello world.</h1>
+    </Head>
+    <main className = {styles.contentContainer}>
+      <section className = {styles.hero}>
+        <span>Hey, welcome</span>  
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get acess to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt="Girl coding" />
+    </main>
     </>
   )
 }
